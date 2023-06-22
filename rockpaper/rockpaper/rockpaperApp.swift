5b1f6a3aa5754078aa4e2b7d90afbdf3301739e6
Sparsh Paliwal <sparshpaliwal123@gmail.com>
2023-06-22 00:30:44 -0400

    Create rock paper scissor

---
+++ rockpaper/rockpaper/rockpaperApp.swift
@@ -0,0 +1,17 @@
+//
+//  rockpaperApp.swift
+//  rockpaper
+//
+//  Created by Sparsh Paliwal on 6/21/23.
+//
+
+import SwiftUI
+
+@main
+struct rockpaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
