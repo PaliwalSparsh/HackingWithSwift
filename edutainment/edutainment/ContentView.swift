11cb2db577d99902ff6c137d2631735130dde69d
Sparsh Paliwal <sparshpaliwal123@gmail.com>
2023-07-08 01:24:11 -0400

    Improve experience

--- edutainment/edutainment/ContentView.swift
+++ edutainment/edutainment/ContentView.swift
@@ -8,6 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    /// TODO -
+    /// 1. split app into structs
+    /// 2. Use Kenney's animations
+    /// 3. Put main buttons into toolbar and turn placement to keyboard.
+    
+    
     let tableRange = 1...10
     
     @State var isGameStarted = false
@@ -16,105 +22,124 @@ struct ContentView: View {
     @State var answer = 0
     @State var rounds = 5
     @State var score = 0
+    @FocusState var isAnswerInputActive: Bool
     var body: some View {
-        if (isGameStarted) {
-            VStack {
-                Button("Main Menu") {
-                    isGameStarted.toggle()
-                }
-                .buttonStyle(.bordered)
-                .padding(20)
-                Text("Your score is \(score)")
-                    .font(.subheadline.bold())
-                    .multilineTextAlignment(.center)
-                    .frame(maxWidth: 300)
-                    .transition(.push(from: .leading))
-                
-                Spacer()
-                
-                Text("Enter the Product of \(practiceNumber)X\(productNumber)")
-                    .font(.title2.bold())
-                    .multilineTextAlignment(.center)
-                    .frame(maxWidth: 300)
-                    .transition(.push(from: .leading))
-                TextField("Enter the Product of \(practiceNumber) X \(productNumber)", value: $answer, format: .number)
-                    .font(.title.bold())
-                    .multilineTextAlignment(.center)
-                    .frame(maxWidth: 300)
-                    .transition(.push(from: .leading))
-                    .foregroundStyle(Color(.systemBlue))
-                
-                Spacer()
-                
-                Button {
-                    // Next round
-                    withAnimation {
-                        if(practiceNumber*productNumber == answer) {
-                            score += 1
-                        } else {
-                            score -= 1
-                        }
-                        
-                        productNumber = tableRange.randomElement() ?? 1
-                        rounds = rounds - 1
-                        
-                        // Game restarts
-                        if(rounds == 0) {
-                            isGameStarted = false
-                            score = 0
-                            rounds = 5
+        NavigationView {
+            if (isGameStarted) {
+                VStack {
+                    Button("Main Menu") {
+                        isGameStarted.toggle()
+                    }
+                    .buttonStyle(.bordered)
+                    .padding(20)
+                    Text("Your score is \(score)")
+                        .font(.subheadline.bold())
+                        .multilineTextAlignment(.center)
+                        .frame(maxWidth: 300)
+                        .transition(.push(from: .leading))
+                    
+                    Spacer()
+                    
+                    Text("Enter the Product of \(practiceNumber)X\(productNumber)")
+                        .font(.title2.bold())
+                        .multilineTextAlignment(.center)
+                        .frame(maxWidth: 300)
+                        .transition(.push(from: .leading))
+                    TextField("Enter the Product of \(practiceNumber) X \(productNumber)", value: $answer, format: .number)
+                        .font(.title.bold())
+                        .multilineTextAlignment(.center)
+                        .frame(maxWidth: 300)
+                        .transition(.push(from: .leading))
+                        .foregroundStyle(Color(.systemBlue))
+                        .focused($isAnswerInputActive)
+                        .keyboardType(.decimalPad)
+                    
+                    Spacer()
+                    
+
+                }.toolbar {
+                    ToolbarItem(placement: .keyboard) {
+                        Button("Done") {
+                            isAnswerInputActive = false
                         }
                     }
-                } label: {
-                    Text(rounds == 1 ? "Last Round": "Next Round")
-                        .font(.title.bold())
-                        .frame(maxWidth: .infinity)
-                }
-                .buttonStyle(.borderedProminent)
-                .padding(20)
-            }
-        } else {
-            VStack {
-                Spacer()
-                Text("Select which table you want to practice?")
-                    .font(.title.bold())
-                    .multilineTextAlignment(.center)
-                    .frame(maxWidth: 300)
-                LazyVGrid(columns: [GridItem(.adaptive(minimum: 56))], spacing: 12) {
-                    ForEach(2..<14) { i in
+                    ToolbarItem(placement: .bottomBar) {
                         Button {
-                            practiceNumber = i
+                            // Next round
+                            startNextRound()
                         } label: {
-                            Text("\(i)")
+                            Text(rounds == 1 ? "Last Round": "Next Round")
                                 .font(.title.bold())
-                                .frame(width: 40, height: 40)
+                                .frame(maxWidth: .infinity)
                         }
-                        .buttonStyle(.bordered)
+                        .buttonStyle(.borderedProminent)
+                        .padding(20)
                     }
-                }.frame(maxWidth: 300)
-                
-                Spacer()
-                    .frame(height: 80)
-                
-                Stepper("\(rounds) Rounds", value: $rounds, in: 1...10)
-                    .font(.title.bold())
-                    .multilineTextAlignment(.center)
-                    .frame(maxWidth: 300)
-                Spacer()
-                
-                Button {
-                    withAnimation() {
-                        isGameStarted = true
-                    }
-                } label: {
-                    Text("Start Game")
-                        .font(.title.bold())
-                        .frame(maxWidth: .infinity)
                 }
-                .buttonStyle(.borderedProminent)
-                .padding(20)
+            } else {
+                VStack {
+                    Spacer()
+                    Text("Select which table you want to practice?")
+                        .font(.title.bold())
+                        .multilineTextAlignment(.center)
+                        .frame(maxWidth: 300)
+                    LazyVGrid(columns: [GridItem(.adaptive(minimum: 56))], spacing: 12) {
+                        ForEach(2..<14) { i in
+                            Button {
+                                practiceNumber = i
+                            } label: {
+                                Text("\(i)")
+                                    .font(.title.bold())
+                                    .frame(width: 40, height: 40)
+                            }
+                            .buttonStyle(.bordered)
+                        }
+                    }.frame(maxWidth: 300)
+                    
+                    Spacer()
+                        .frame(height: 80)
+                    
+                    Stepper("\(rounds) Rounds", value: $rounds, in: 1...10)
+                        .font(.title.bold())
+                        .multilineTextAlignment(.center)
+                        .frame(maxWidth: 300)
+                    Spacer()
+                    
+                    Button {
+                        withAnimation() {
+                            isGameStarted = true
+                            isAnswerInputActive = true
+                        }
+                    } label: {
+                        Text("Start Game")
+                            .font(.title.bold())
+                            .frame(maxWidth: .infinity)
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .padding(20)
+                }
+                .transition(.opacity)
             }
-            .transition(.opacity)
+        }
+    }
+    func startNextRound() {
+        withAnimation {
+            if(practiceNumber*productNumber == answer) {
+                score += 1
+            } else {
+                score -= 1
+            }
+            
+            productNumber = tableRange.randomElement() ?? 1
+            rounds = rounds - 1
+            isAnswerInputActive = true
+            
+            // Game restarts
+            if(rounds == 0) {
+                isGameStarted = false
+                score = 0
+                rounds = 5
+            }
         }
     }
 }
