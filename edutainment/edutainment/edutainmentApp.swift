1db78b00c5c9785eeb4a04e77127d80751e6e9da
Sparsh Paliwal <sparshpaliwal123@gmail.com>
2023-07-08 00:58:18 -0400

    Base app - no animations or extra features

---
+++ edutainment/edutainment/edutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  edutainmentApp.swift
+//  edutainment
+//
+//  Created by Sparsh Paliwal on 7/7/23.
+//
+
+import SwiftUI
+
+@main
+struct edutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
