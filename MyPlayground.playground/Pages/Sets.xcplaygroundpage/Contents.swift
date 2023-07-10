211ab9933e85211cc072c2a00857ff4f5dc21caa
Sparsh Paliwal <sparshpaliwal123@gmail.com>
2023-07-10 01:18:46 -0400

    Base

--- MyPlayground.playground/Pages/Sets.xcplaygroundpage/Contents.swift
+++
@@ -1,9 +0,0 @@
-//: [Previous](@previous)
-
-import Foundation
-
-// Sets don't have an order, they can't have duplicate, however they are super fast at lookup. Arrays vs Sets – Because sets don’t need to store your objects in the order you add them(like arrays), they can instead store them in a seemingly random order that optimizes them for fast retrieval.
-var actors = Set<String>()
-actors.insert("SRK")
-actors.insert("Salman")
-actors.insert("Munna bhai")
