7e39c12c0b9dd0b012344c4958917ee387622601
Sparsh Paliwal <sparshpaliwal123@gmail.com>
2023-08-04 19:38:42 -0400

    challenge

--- moonshot/moonshot/ContentView.swift
+++ moonshot/moonshot/ContentView.swift
@@ -7,44 +7,68 @@
 
 import SwiftUI
 
+struct MissionGrid: View {
+    var missions: [Mission]
+    var astronauts: [String: Astronaut]
+    
+    var body: some View {
+        ScrollView {
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 150))]) {
+                ForEach(missions) { mission in
+                    NavigationLink {
+                        MissionView(mission: mission, astronauts: astronauts)
+                    } label: {
+                        VStack {
+                            Image(mission.image)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 100, height: 100)
+                            VStack {
+                                Text(mission.displayName)
+                                    .font(.subheadline.bold())
+                                Text(mission.launchDate ?? "N/A")
+                            }
+                            .frame(maxWidth: .infinity)
+                            
+                        }
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(.lightBackground)
+                        .clipShape(RoundedRectangle(cornerRadius: 12))
+                    }
+                }
+            }
+            .padding(.horizontal)
+        }
+    }
+}
+
 struct ContentView: View {
+    @State var showList = true
+    
     let missions: [Mission] = Bundle.main.decode("missions.json")
     let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
     
     var body: some View {
         NavigationView {
-            ScrollView {
-                LazyVGrid(columns: [GridItem(.adaptive(minimum: 150))]) {
-                    ForEach(missions) { mission in
-                        NavigationLink {
-                            MissionView(mission: mission, astronauts: astronauts)
-                        } label: {
-                            VStack {
-                                Image(mission.image)
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 100, height: 100)
-                                VStack {
-                                    Text(mission.displayName)
-                                        .font(.subheadline.bold())
-                                    Text(mission.launchDate ?? "N/A")
-                                }
-                                .frame(maxWidth: .infinity)
-
-                            }
-                            .padding()
-                            .foregroundColor(.white)
-                            .background(.lightBackground)
-                            .clipShape(RoundedRectangle(cornerRadius: 12))
-                        }
+            VStack {
+                if(showList) {
+                    List(missions) { mission in
+                        NavigationLink(mission.displayName, destination: MissionView(mission: mission, astronauts: astronauts))
                     }
+                } else {
+                    MissionGrid(missions: missions, astronauts: astronauts)
                 }
-                .padding(.horizontal)
             }
             .background(.darkBackground)
             .navigationTitle("Moonshot")
             /// We can force a particular color scheme on a view, this way all iOS elements such as nav bars and other stuff will turn light on dark based on what you define.
             .preferredColorScheme(.dark)
+            .toolbar {
+                    Button(showList ? "See Grid": "See List") {
+                        showList.toggle()
+                    }
+                }
         }
     }
 }
