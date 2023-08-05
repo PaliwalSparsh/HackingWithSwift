29cca9fe87d1ab4427d6ffaaac21acfd22dbae7a
Sparsh Paliwal <sparshpaliwal123@gmail.com>
2023-08-04 23:38:58 -0400

    wordscramble app

---
+++ wordScramble/wordScramble/wordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  wordScrambleApp.swift
+//  wordScramble
+//
+//  Created by Sparsh Paliwal on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct wordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
