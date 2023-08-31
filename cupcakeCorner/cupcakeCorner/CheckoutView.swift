eff0e4e50f003463aa9b2ff89dab5b3f87f6a6d1
Sparsh Paliwal <sparshpaliwal123@gmail.com>
2023-08-31 00:25:58 -0400

    Finish Cupcake Corner

--- cupcakeCorner/cupcakeCorner/CheckoutView.swift
+++ cupcakeCorner/cupcakeCorner/CheckoutView.swift
@@ -10,10 +10,15 @@ import SwiftUI
 struct CheckoutView: View {
     @ObservedObject var order: Order
     
+    @State var alertMessage = ""
+    @State var showAlertMessage = false
+    
     var body: some View {
         ScrollView {
             VStack {
+                /// AsyncImage allows us to load image in async manner
                 AsyncImage(url: URL(string: "https://hws.dev/img/cupcakes@3x.jpg"), scale: 3) { image in
+                    /// We can add modifier to an aysnc image only after its added, one of the call definitision of asyncImage also allows us to check phases in which image download is.
                         image
                             .resizable()
                             .scaledToFit()
@@ -22,12 +27,46 @@ struct CheckoutView: View {
                 }
                 Text("Your total cost is \(order.cost, format: .currency(code: "USD"))")
                 Button("Checkout") {
-                    
+                    /// Inside a button to run an async function we need to use Task {} closure.
+                    Task {
+                        await placeOrder()
+                    }
                 }
                 .padding()
             }
         }
-
+        .navigationTitle("Checkout")
+        .navigationBarTitleDisplayMode(.inline)
+        .alert("Thankyou", isPresented: $showAlertMessage) {
+            Button("OK") {}
+        } message: {
+            Text(alertMessage)
+        }
+    }
+    
+    // Making HTTP request in SWift
+    func placeOrder() async {
+        /// 1. Encode the data
+        guard let encodedData = try? JSONEncoder().encode(order) else {
+            print("Unable to encode order")
+            return
+        }
+        
+        /// 2. Create URL Request
+        let url = URL(string: "https://reqres.in/api/cupcakes")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        
+        do {
+            /// 3. Start URL session and do a POST or GET. Here we are doing POST request.
+            let (data, _) = try await URLSession.shared.upload(for: request, from: encodedData)
+            showAlertMessage = true
+            let decodedData = try JSONDecoder().decode(Order.self, from: data)
+            alertMessage = "Your order for \(decodedData.quantity) cupcakes is done."
+        } catch {
+            print("Checkout Failed")
+        }
     }
 }
 
--- cupcakeCorner/cupcakeCorner/Order.swift
+++ cupcakeCorner/cupcakeCorner/Order.swift
@@ -7,7 +7,16 @@
 
 import Foundation
 
-class Order: ObservableObject {
+/// For a class which has @Published properties in it, we cannot just directly add Codable and expect
+/// the class to be decoded and encoded automatically, instead we need to tell swift how the encoding and
+/// decoding will happen
+class Order: ObservableObject, Codable {
+    
+    /// We define the CodingKeys enum for use in encoding and decoding functions
+    enum CodingKeys: CodingKey {
+        case type, quantity, hasSprinkles, hasFrosting, name, street, city, zip
+    }
+    
     static let types = ["Strawberry", "Mango", "Rainbow", "Chocolate"]
     
     @Published var quantity = 3
@@ -28,6 +37,7 @@ class Order: ObservableObject {
     @Published var city = ""
     @Published var zip = ""
     
+    /// Through such computed properties we can introduce validations alongside data
     var hasValidAddress: Bool {
         if(name.isEmpty || street.isEmpty || city.isEmpty || zip.isEmpty) {
             return false
@@ -44,5 +54,43 @@ class Order: ObservableObject {
             cost += Double(quantity) * 0.5
         }
         return cost
+    }
+    
+    /// This is an empty init, in Swift a class can have multiple inits and class can be initialised using any of those inits.
+    init() {}
+
+    /// This init with decoder contains the strategy for how class will be decoded if needed. We start by creating a container
+    /// and saying the container to be keyed by the enum CodingKeys. Then for each key we add value.
+    /// Required in front of init means if some class inherits this class then it would need to create an override init function.
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        type = try container.decode(Int.self, forKey: .type)
+        quantity = try container.decode(Int.self, forKey: .quantity)
+        
+        hasSprinkles = try container.decode(Bool.self, forKey: .hasSprinkles)
+        hasFrosting = try container.decode(Bool.self, forKey: .hasFrosting)
+        
+        name = try container.decode(String.self, forKey: .name)
+        street = try container.decode(String.self, forKey: .street)
+        city = try container.decode(String.self, forKey: .city)
+        zip = try container.decode(String.self, forKey: .zip)
+    }
+    
+    /// Similar to decoder in the encode function we define how encoding should happen, we again create a container
+    /// and tell how keys should be added.
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        
+        try container.encode(type, forKey: .type)
+        try container.encode(quantity, forKey: .quantity)
+        
+        try container.encode(hasSprinkles, forKey: .hasSprinkles)
+        try container.encode(hasFrosting, forKey: .hasFrosting)
+        
+        try container.encode(name, forKey: .name)
+        try container.encode(street, forKey: .street)
+        try container.encode(city, forKey: .city)
+        try container.encode(zip, forKey: .zip)
     }
 }
