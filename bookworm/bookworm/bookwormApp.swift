081a6a20ef252d00938ca6d7945475978e8f3e52
Sparsh Paliwal <sparshpaliwal123@gmail.com>
2023-09-06 18:08:51 -0400

    bookworm

---
+++ bookworm/bookworm/bookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  bookwormApp.swift
+//  bookworm
+//
+//  Created by Sparsh Paliwal on 9/1/23.
+//
+
+import SwiftUI
+
+@main
+struct bookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
